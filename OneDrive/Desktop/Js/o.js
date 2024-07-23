86521a154e99a9eb61e2d2951d9e0d8603289e7f
Mihir Vetal <151329052+Mihirvetal@users.noreply.github.com>
2024-07-23 23:37:51 +0530

    c

---
+++ OneDrive/Desktop/Js/o.js
@@ -0,0 +1,29 @@
+// function abc(a){
+//     if (a<18){
+//         console.log("Minor");
+//     }else{
+//         console.log("Adult");
+//     }
+// }
+// abc(18);
+
+// function multiply(num1, num2) {
+//     let result = num1 * num2;
+//     return result;
+//   }
+
+// console.log(multiply(1 ,2))
+
+// forloop
+
+let m = ["m","i","h","i","r"];
+
+let str="";
+function string(s){
+    for(let i=0;i<m.length;i++){
+        str=str+ m[i]
+    
+    }return str
+
+}
+console.log(string(m))
